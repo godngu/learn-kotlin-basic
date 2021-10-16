2a60cbd30238ad3019dc3f12a1d57aeffec4c06a
kil <godngu@gmail.com>
2021-10-16 15:28:37 +0900

    finished Chapter02

---
+++ src/chap02/Exception.kt
@@ -0,0 +1,21 @@
+package chap02
+
+import java.io.BufferedReader
+import java.io.StringReader
+import java.lang.NumberFormatException
+
+fun readNumber(reader: BufferedReader) : Int? {
+    return try {
+        val line = reader.readLine()
+        Integer.parseInt(line)
+    } catch (e: NumberFormatException) {
+        null
+    } finally {
+        reader.close()
+    }
+}
+
+fun main() {
+    println(readNumber((BufferedReader(StringReader("239")))))
+    println(readNumber((BufferedReader(StringReader("not a number")))))
+}
