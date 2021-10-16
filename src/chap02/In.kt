ddb0493e692f5e2540e075ce1d9d82c0da0ee3b3
kil <godngu@gmail.com>
2021-10-16 12:40:03 +0900

    in

---
+++ src/chap02/In.kt
@@ -0,0 +1,19 @@
+package chap02
+
+fun main() {
+    println(isLetter('q'))
+    println(isNotDigit('x'))
+    println(isNotDigit('1'))
+    println(recognize('C'))
+    println(recognize('#'))
+    println("Kotlin" in "Java".."Scala")
+}
+
+fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+fun isNotDigit(c: Char) = c !in '0'..'9'
+
+fun recognize(c: Char) = when (c) {
+    in '0'..'9' -> "It's a digit!"
+    in 'a'..'z', in 'A'..'Z' -> "It's a letter!"
+    else -> "I don't know.."
+}
